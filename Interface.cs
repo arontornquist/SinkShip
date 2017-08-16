04ac0b16fa139f97a7307e5ba51e4df123ff1f1a
STENUNGSUND\gustav.themer <gustav@themer.se>
2017-08-16 16:50:49 +0200

    Har utökat menyn och lagt till nya - i Program, NewGame() , i Interface, AskForInt()

--- Interface.cs
+++ Interface.cs
@@ -14,6 +14,33 @@ namespace SinkShip
             return Console.ReadLine();
         }
 
+        public int AskForInt(string output)
+        {
+            int answer = 0;
+            bool isInt = false;
+            do
+            {
+                Console.Write(output);
+                try
+                {
+                    answer = int.Parse(Console.ReadLine());
+                    isInt = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Skriv in ett nummer, försök igen");
+                }
+            } while (!isInt);
+            return answer;
+        }
+
+        internal void DisplayHighScore()
+        {
+            //TODO: Skriv metoden!
+            //throw new NotImplementedException();
+            Console.WriteLine("High Score Lista");
+        }
+
         public void DisplayMenu()
         {
             Console.WriteLine("-- Sänka Skepp --");
